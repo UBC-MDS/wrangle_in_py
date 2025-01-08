caed911c08e1a38ce4a3929e721f01c6257c0a0f
stephta <stephanieta@hotmail.ca>
2025-01-07 21:43:27 -0800

    add params description, output description, and example to docstring

--- src/wranglepy/column_standardizer.py
+++ src/wranglepy/column_standardizer.py
@@ -2,7 +2,25 @@ def column_standardizer(dataframe):
     """
     Returns a copy of the inputted dataframe with standardized column names.
     Column names will be converted to lowercase and
-    non-alphanumerics or spaces will be replaced with underscores.
+    non-alphanumerics (including spaces and puncutation) will be replaced with underscores.
 
+    Parameters
+    ----------
+    dataframe : pd.DataFrame
+        The input pandas DataFrame whose column names need standardization.
 
+    Returns
+    -------
+    pd.DataFrame
+        A new DataFrame with standardized column names.
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> data = {'Jack Fruit 88': [1, 2], 'Pineapples!': [3, 4], 'Dragon (Fruit)': [25, 30]}
+    >>> df = pd.DataFrame(data)
+    >>> column_standardizer(df)
+       jack_fruit_88  pineapples  dragon_fruit
+    0           1          3         25
+    1           2          4         30
     """
