593d78e56bfe0c1757639f7d9357139778e15a5f
stephta <stephanieta@hotmail.ca>
2025-01-07 21:28:59 -0800

    write function definition and docstring description

---
+++ src/wranglepy/column_standardizer.py
@@ -0,0 +1,8 @@
+def column_standardizer(dataframe):
+    """
+    Returns a copy of the inputted dataframe with standardized column names.
+    Column names will be converted to lowercase and
+    non-alphanumerics or spaces will be replaced with underscores.
+
+
+    """
